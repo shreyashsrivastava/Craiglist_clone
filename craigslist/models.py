78ffc8c36861b0ba5aec1b2ad5706592128e4285
shreyashsrivastava <shreyash.rocks.ever.10231@g>
2020-06-24 17:22:40 +0530

    added basic functionality

--- craigslist/models.py
+++ craigslist/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Search(models.Model):
-    search = models.CharField(max_length=200)
+    search = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    class Meta:
+        verbose_name_plural = 'Searches'
--- craigslist/views.py
+++ craigslist/views.py
@@ -1,6 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render
+from requests.compat import quote_plus
+from . import models
+import urllib
 
-# Create your views here.
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 
 def home(request):
-    return render(request,'craigslist/index.html')
+    return render(request,'craigslist/base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+        
+        final_postings.append((post_title, post_url, post_price))
+
+
+    for_front = {
+        'search' : search,
+        'final_postings' : final_postings,
+    }
+    return render(request, 'craigslist/new_search.html', for_front)
